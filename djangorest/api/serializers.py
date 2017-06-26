d59e118d51935a729916589b645f72d0b283ca60
Bill Mike <billmike1994@gmail.com>
2017-06-26 18:58:05 +0100

    First commit

---
+++ djangorest/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Bucketlist
+
+class BucketlistSerializer(serializers.ModelSerializer):
+	"""docstring for BucketlistSerializer"""
+	owner = serializers.ReadOnlyField(source = 'owner.username')
+
+
+	class Meta:
+		"""docstring for meta"""
+		model = Bucketlist
+		fields = ('id', 'name', 'owner', 'date_created', 'date_modified')
+		read_only_fields = ('date_created', 'date_modified')
+			
+		
